8794fceafa134a457032316f632cd8c83d130db5
hwoolen03 <124072567+hwoolen03@users.noreply.github.com>
2025-01-15 02:54:05 +0000

    Update User.js

--- User.js
+++ User.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String },
+    authProvider: { type: String, required: true } // e.g., 'github', 'google', 'facebook'
 });
 
 module.exports = mongoose.model('User', userSchema);
