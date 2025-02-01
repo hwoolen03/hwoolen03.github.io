b6b3a983d7005de9f938df795f692c37a3324238
hwoolen03 <124072567+hwoolen03@users.noreply.github.com>
2025-02-01 17:14:56 +0000

    Update account.js

--- account.js
+++ account.js
@@ -1,11 +1,8 @@
-import * as tf from './node_modules/@tensorflow/tfjs';
-
-let auth0Client = null;
-
 const API_HEADERS = {
-    'x-rapidapi-key': '4fbc13fa91msh7eaf58f815807b2p1d89f0jsnec07b5b547c3',
+    'x-rapidapi-key': 'YOUR_API_KEY_HERE',
     'x-rapidapi-host': 'booking-com15.p.rapidapi.com'
 };
+
 // Auth0 Configuration
 const configureClient = async () => {
     try {
