0fdd22e844e8de8361a77aede3de5995b1a9eb0e
hwoolen03 <124072567+hwoolen03@users.noreply.github.com>
2025-02-04 00:27:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
     console.log("DOMContentLoaded event fired");
     let auth0Client = null;
-    const redirectUri = "https://hwoolen03.github.io";
+    const redirectUri = window.location.origin + "/indexsignedin.html"; // Updated redirect URI
 
     const configureClient = async () => {
         console.log("Configuring Auth0 client...");
